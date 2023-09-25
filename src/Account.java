2e361039a8b1281a0e185a1a46d1780697cb012e
GEL2345 <G.E.Loake@lse.ac.uk>
2023-09-25 10:06:32 +0100

    First Commit

--- src/Account.java
+++ src/Account.java
@@ -5,6 +5,23 @@ public class Account {
     public void printDetails() {
         System.out.println(username + " , " + password);
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public static void main(String[] args) {
         Account a = new Account();
         a.username = "John1";
